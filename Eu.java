ddad38b9c3189c2a9af7f12761fa5d1039d7300a
bigcido <130413132+bigcido@users.noreply.github.com>
2024-10-23 02:13:47 -0300

    Create Eu.java
    Item 02

---
+++ Eu.java
@@ -0,0 +1,13 @@
+public class Eu {
+  public static void main (String [] args){
+    while (true){
+      try{
+        System.out.println("Alvaro);
+        Thread.sleep(300000);
+      catch (InterruptedException e){
+        e.PrintStackTrace();
+      }
+  }
+}
+}
+}
